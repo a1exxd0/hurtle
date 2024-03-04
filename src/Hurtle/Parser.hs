33353f12111e9fb6f88e0d58356019dd639ad723
Alex Do <alexxdo2004@gmail.com>
2024-03-04 11:28:00 +0000

    satisfy ignore case blah

--- src/Hurtle/Parser.hs
+++ src/Hurtle/Parser.hs
@@ -5,6 +5,46 @@ import Hurtle.Types
 -- You'll probably want to refer to https://hackage.haskell.org/package/megaparsec for documentation of the Megaparsec library.
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import Data.Char
+
+-- | Helpers
+
+satisfyIgnoreCase :: (String -> Bool) -> Parser String
+satisfyIgnoreCase f = takeWhileP (Just "string") (\s -> f (map toLower s))
+
+-- | Variable Parsers
+
+parseVariableByName :: Parser Variable
+parseVariableByName = do
+    _ <- satisfy (==':')
+    name <- takeWhileP (Just "character") (\c -> c /= ' ' && c /= '\n' && c /= ',')
+    pure (Variable $ Left $ map toLower name)
+
+parseVariableByValue :: Parser Variable
+parseVariableByValue = do
+    num <- some digitChar
+    _ <- char '.'
+    decimal <- some digitChar
+    _ <- char ' '
+    pure $ Variable $ Right (read (num ++ "." ++ decimal) :: Float)
+  <|> do
+    num <- some digitChar
+    _ <- char ' '
+    return $ Variable $ Right (read num :: Float)
+
+parseByNameByValue :: Parser Variable
+parseByNameByValue = parseVariableByName <|> parseVariableByValue
+
+parseSum :: Parser Variable
+parseSum = do
+    _ <- satisfyIgnoreCase (=="sum")
+    _ <- skipMany space
+    var1 <- parseByNameByValue
+    _ <- skipMany space
+    var2 <- parseByNameByValue
+    _ <- takeWhileP (Just "whitespace") (\c -> c /= ' ' && c /= '\n' && c /= ',')
+    pure $ Sum var1 var2
+
 
 parseHogo :: Parser HogoProgram
 parseHogo = error "Implement me :)"
--- src/Hurtle/Types.hs
+++ src/Hurtle/Types.hs
@@ -2,27 +2,44 @@ module Hurtle.Types where
 
 import Text.Megaparsec
 import Data.Void
+import Data.Map
 
 --------------------------------------------------------------------------------
 -- Type Definitions
 
--- | A Hogo program is a list of HogoCode instructions.
-type HogoProgram = [HogoCode]
+-- | A Hogo program is a list of HogoCode instructions
+data HogoProgram = HogoProgram {
+  varTable :: Map String (Either Variable Float),
+  procTable :: Map String ([String], HogoProgram),
+  code     :: [HogoCode]
+  } 
+  deriving (Show, Read, Eq)
+
+data Variable 
+  = Variable (Either String Float)
+  | Sum Variable Variable
+  | Difference Variable Variable
+  | Multiply Variable Variable
+  deriving (Show, Read, Eq)
 
 data HogoCode
   -- | Movement Commands
-  = GoForward Float
-  | GoBackward Float
-  | TurnLeft Float
-  | TurnRight Float
-  | GoHome
+  = Forward Variable
+  | Backward Variable
+  | GoLeft Variable
+  | GoRight Variable
+  | Home
   -- | Pen Commands
   | PenUp
   | PenDown
+  | SetWidth Variable
+  | SetColor Variable
   | ClearScreen
   -- | Control Flow
-  | Repeat Int HogoProgram
+  | For HogoProgram
+  | Function String
   deriving (Show,Read,Eq)
+
 
 -- | This is an alias for the Megaparsec parser type; the "Void" tells it that we don't have any custom error type, and the "string" tells it that we're parsing strings.
 type Parser = Parsec Void String
